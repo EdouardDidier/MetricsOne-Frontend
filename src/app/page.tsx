e075e75b37e8120fbd413ac49f0dd9d977d5ab44
Edouard DIDIER <zeddydy@hotmail.com>
2025-04-22 11:26:59 +0200

    Update index page with simple welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,93 +2,9 @@ export default function Page() {
   return (
     <div>
       <title>F1Metrics - Home</title>
-      <p className="w-xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius
-        feugiat sapien ac mollis. Sed finibus viverra nisl, eget viverra orci
-        fermentum non. Sed tellus massa, dignissim et dui eu, hendrerit rutrum
-        arcu. Cras euismod sagittis metus, nec congue lorem posuere varius.
-        Fusce non nibh ultrices, venenatis purus ac, eleifend sem. Orci varius
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-        mus. Donec a lobortis massa, ut pharetra nulla. Integer eu libero nunc.
-        Sed maximus metus vitae dolor vehicula, vitae imperdiet ligula lacinia.
-        Curabitur viverra tellus eu nisl fermentum blandit. Donec cursus justo
-        commodo tellus pharetra mattis. Class aptent taciti sociosqu ad litora
-        torquent per conubia nostra, per inceptos himenaeos. Phasellus sagittis
-        elit massa, eget commodo diam sollicitudin id. Sed elementum, ipsum eu
-        tempor ullamcorper, arcu mi vulputate metus, ut elementum felis erat
-        porta turpis. Proin ullamcorper nibh lacus, vitae mollis nisl varius at.
-        Vivamus nec rhoncus tortor. Vestibulum eu posuere metus. Nulla laoreet
-        massa ut tellus aliquet lacinia. Donec pretium nulla sed mi vehicula,
-        non condimentum tellus sollicitudin. Pellentesque sed est id risus
-        semper venenatis in at orci. Vestibulum fermentum nulla sit amet nulla
-        tempor, in consectetur eros dapibus. Praesent elementum eros eu mattis
-        aliquet. Vestibulum vel metus bibendum ligula venenatis vehicula.
-        Integer pellentesque posuere leo nec posuere. Donec placerat tortor eget
-        efficitur tempus. Mauris dictum ante in libero lacinia gravida. Sed
-        neque sapien, cursus ac nulla nec, aliquam facilisis libero.
-        Pellentesque habitant morbi tristique senectus et netus et malesuada
-        fames ac turpis egestas. Sed in felis volutpat lectus consequat
-        consequat quis non enim. Integer vel purus sit amet mauris varius
-        finibus vel eu dolor. Praesent commodo non massa eget facilisis.
-        Praesent non diam ut sapien consectetur tincidunt. Vivamus maximus nunc
-        massa, in mollis tortor semper sollicitudin. Vestibulum dolor risus,
-        fermentum ac efficitur at, finibus eget enim. Curabitur sodales nisi vel
-        leo blandit feugiat ut sed massa. Sed sodales felis ac turpis semper,
-        ultricies volutpat turpis ullamcorper. Vestibulum dictum tristique
-        justo, eu congue justo. Donec nec felis felis. Sed interdum, nulla et
-        efficitur pellentesque, dolor tortor laoreet enim, eu sagittis ipsum
-        neque in arcu. Vivamus non erat consequat, imperdiet odio id, volutpat
-        turpis. Donec fermentum gravida ante. Morbi nec nunc at enim porttitor
-        pharetra. Cras luctus enim at orci vestibulum maximus id eget tellus.
-        Sed ac magna tincidunt, sodales est a, semper ex. Cras vestibulum ipsum
-        sed eros accumsan fermentum. Suspendisse sollicitudin sem purus, vel
-        viverra odio sagittis non. Praesent nunc mi, semper vel fermentum non,
-        egestas a felis. Curabitur eu imperdiet leo, quis efficitur magna. Proin
-        faucibus elit ipsum, in feugiat arcu accumsan vitae. Ut eget ante
-        euismod, consequat quam in, fringilla sapien. Etiam sit amet ipsum non
-        velit suscipit consequat. Suspendisse id aliquet arcu. In eu semper sem,
-        in tempus justo. Curabitur porttitor, turpis vitae sagittis viverra,
-        sapien sapien porta odio, quis ultricies lacus augue eu erat. Sed vitae
-        sem quis lectus faucibus facilisis nec vel ipsum. Aenean mattis lobortis
-        augue, at tempus tortor commodo id. Mauris aliquam sit amet nunc eget
-        rutrum. Morbi non massa libero. Phasellus aliquam nibh congue ex
-        interdum, at tempus dui semper. Mauris et sapien tortor. Praesent
-        tincidunt at urna a malesuada. Nam sapien nulla, feugiat ac orci ac,
-        laoreet gravida dui. Quisque nec consectetur leo. Proin bibendum turpis
-        nulla, eget porta dolor consectetur at. Suspendisse potenti. Nunc id
-        luctus felis, non mollis magna. Ut maximus purus non pharetra lacinia.
-        Pellentesque eu pharetra mauris. Donec vitae pharetra arcu, nec eleifend
-        nulla. Nam lacinia tortor at tellus ultricies iaculis. Sed tempor neque
-        quis tortor scelerisque volutpat. Duis scelerisque ullamcorper dui, non
-        vulputate quam porta et. Quisque egestas iaculis ante, nec porta dolor
-        condimentum eget. Ut eu lorem semper, egestas massa sed, iaculis mi.
-        Fusce faucibus velit pharetra nisi posuere ullamcorper. Proin faucibus
-        lorem non libero ultricies, vel facilisis erat lobortis. Cras maximus
-        ornare eros, vel eleifend nunc interdum porta. Sed maximus felis massa,
-        eu blandit erat aliquam eget. Curabitur tempus nisl in ex tempor, eget
-        egestas odio elementum. Proin sollicitudin quam eu libero lacinia, id
-        dignissim eros condimentum. Sed elementum efficitur orci, vel ultricies
-        augue tincidunt a. Morbi eget ipsum nec leo varius volutpat. Vestibulum
-        at tempus risus, sit amet consequat ex. Donec at ultricies sapien. Morbi
-        posuere tincidunt sem, sit amet eleifend tellus fringilla ut. Fusce
-        fringilla nibh nec libero hendrerit, vitae cursus odio tincidunt. Etiam
-        quis urna eget ante interdum ultricies quis ornare ante. Sed dui diam,
-        semper pulvinar mauris eu, mattis maximus turpis. Suspendisse laoreet
-        aliquet ornare. Morbi varius faucibus tristique. Morbi sodales, arcu
-        quis maximus lacinia, erat nisl vehicula dolor, tincidunt convallis
-        justo sapien at velit. Donec elementum vulputate est eu aliquam. Sed
-        suscipit massa quam, et congue ex tincidunt id. Donec molestie ultricies
-        lorem, sit amet faucibus odio. Mauris placerat efficitur metus id
-        tempor. Aenean sollicitudin aliquet est, et convallis metus pulvinar
-        sed. Praesent tortor sem, sodales ut molestie eu, convallis non nisi.
-        Aenean lorem massa, volutpat sed ornare sed, ultricies nec nisi. Etiam
-        vitae tincidunt dolor. Aliquam quis eros vel metus maximus lacinia.
-        Aliquam euismod porttitor lacus quis imperdiet. Donec semper ante et
-        lobortis tempus. Nam in nibh id velit euismod fringilla a non orci.
-        Integer ligula orci, ultricies a pulvinar vitae, condimentum ut nisl.
-        Curabitur faucibus leo id tortor feugiat volutpat. Vivamus consectetur
-        congue accumsan.
-      </p>
+      <h1 className="font-bold text-4xl text-center mt-48">
+        Welcome to Metrics One
+      </h1>
     </div>
   );
 }
