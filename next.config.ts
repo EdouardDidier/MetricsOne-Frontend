8c50241c949db3bb15d443645c263a7b1ece2325
Edouard DIDIER <zeddydy@hotmail.com>
2025-04-24 22:02:00 +0200

    Add environment variables for API URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_HOST: process.env.API_HOST,
+    API_PORT: process.env.API_PORT,
+  },
 };
 
 export default nextConfig;
