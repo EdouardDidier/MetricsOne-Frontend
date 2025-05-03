a7bc4c877815f614810d28735f498f0409936352
Edouard DIDIER <zeddydy@hotmail.com>
2025-05-03 12:55:41 +0200

    Update Drivers page to new API specs

--- src/app/drivers/page.tsx
+++ src/app/drivers/page.tsx
@@ -1,28 +1,43 @@
 import { Driver } from "@/types/Driver";
+import Image from "next/image";
 
 export default async function Page() {
   const response = await fetch(
-    "https://api.openf1.org/v1/drivers?session_key=latest",
+    process.env.API_HOST +
+      ":" +
+      process.env.API_PORT +
+      "/drivers?expand=team,images",
   );
   const data: Array<Driver> = await response.json();
+  // TODO: Handle API error
 
   return (
     <div>
       <title>F1Metrics - Drivers</title>
       <h1>F1 Drivers 2025</h1>
       <div className="flex flex-row flex-wrap">
-        {data.map((driver) => (
-          <div
-            className={`
+        {data.map((driver) => {
+          const full_name =
+            driver.first_name + " " + driver.last_name.toUpperCase();
+
+          return (
+            <div
+              className={`
               border-solid border-1 border-gray-300
               m-1 p-2 w-32
             `}
-            key={driver.full_name}
-          >
-            <img src={driver.headshot_url} alt={driver.full_name} />
-            {driver.driver_number} {driver.full_name}
-          </div>
-        ))}
+              key={full_name}
+            >
+              <Image
+                src={process.env.IMAGE_URL + driver.images?.headshot_url}
+                alt={`Picture of ${full_name}`}
+                width={840}
+                height={840}
+              />
+              {driver.number} {full_name}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
