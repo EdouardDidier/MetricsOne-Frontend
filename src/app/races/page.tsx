6ecb00de45ad56ae3f4d512e22995a7133782ff1
Edouard D. <zeddydy@hotmail.com>
2025-06-05 16:29:45 +0200

    Remove 'sessions' from Races page

--- src/app/races/page.tsx
+++ src/app/races/page.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 
 export default async function Page() {
   const response = await fetch(
-    process.env.API_HOST +
-      ":" +
-      process.env.API_PORT +
-      "/2025/meetings?expand=sessions",
+    process.env.API_HOST + ":" + process.env.API_PORT + "/2025/meetings",
   );
 
   const data: Array<Meeting> = await response.json();
@@ -17,30 +14,15 @@ export default async function Page() {
       <title>F1Metrics - Races</title>
       <h1>Races</h1>
       <div>
-        <dl>
-          {data.map((meeting) => {
-            if (meeting.sessions == null) {
-              return "Error";
-            }
-
-            return (
-              <div key={meeting.name}>
-                <dt>
-                  <Link href={`/races/${meeting.location.toLowerCase()}`}>
-                    {meeting.name}
-                  </Link>
-                </dt>
-                {meeting.sessions.map((session) => {
-                  return (
-                    <dd
-                      key={meeting.name + session.name}
-                    >{`- ${session.name}`}</dd>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </dl>
+        {data.map((meeting) => {
+          return (
+            <div key={meeting.name}>
+              <Link href={`/races/${meeting.location.toLowerCase()}`}>
+                {meeting.name}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
